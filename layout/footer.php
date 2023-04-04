2560cede2bf5a1138903dfae6a09b29b1bac6ab0
rakibbd24 <123587353+rakibbd24@users.noreply.github.com>
2023-04-04 22:23:14 +0600

    Update footer.php

--- layout/footer.php
+++ layout/footer.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="site-info">
 			<h1 style="font-family: 'Herr Von Muellerhoff';color: #ccc;font-weight:300;text-align: center;margin-bottom:0;margin-top:0;line-height:1.4;font-size: 46px;">Moschino</h1>
-			 <a target="blank" href="https://www.wowthemes.net/">&copy; ostad blog</a>
+			 <a target="blank" href="">&copy; ostad blog</a>
 		</div>
 	</div>	
 	</footer>
