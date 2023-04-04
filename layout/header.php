eb6f0ef24717892f7c02aed60981ae93aa814a9b
Rakib ahmed <smjasim24@gmail.com>
2023-04-04 15:51:46 +0600

    Initial commit

---
+++ layout/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Ostad blog website</title>
+<link rel='stylesheet' href='assets/css/woocommerce-layout.css' type='text/css' media='all'/>
+<link rel='stylesheet' href='assets/css/woocommerce-smallscreen.css' type='text/css' media='only screen and (max-width: 768px)'/>
+<link rel='stylesheet' href='assets/css/woocommerce.css' type='text/css' media='all'/>
+<link rel='stylesheet' href='assets/css/font-awesome.min.css' type='text/css' media='all'/>
+<link rel='stylesheet' href='assets/css/style.css' type='text/css' media='all'/>
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Oswald:400,500,700%7CRoboto:400,500,700%7CHerr+Von+Muellerhoff:400,500,700%7CQuattrocento+Sans:400,500,700' type='text/css' media='all'/>
+<link rel='stylesheet' href='assets/css/easy-responsive-shortcodes.css' type='text/css' media='all'/>
+</head>
+<body class="single single-post">
+<div id="page">
+	<div class="container">
+		<header id="masthead" class="site-header">
+		<div class="site-branding">
+			<h1 class="site-title"><a href=/blog rel="home">Morning blog</a></h1>
+			<h2 class="site-description">Everydat with update</h2>
+		</div>
+		<nav id="site-navigation" class="main-navigation">
+		<button class="menu-toggle">Menu</button>
+		<a class="skip-link screen-reader-text" href="#content">Skip to content</a>
+		<div class="menu-menu-1-container">
+			<ul id="menu-menu-1" class="menu">
+				<li><a href="http://localhost/blog/">Home</a></li>
+				<li><a href="about.html">About</a></li>
+				<li><a href="shop.html">Shop</a></li>
+				<li><a href="blog.html">Blog</a></li>
+				<li><a href="elements.html">Elements</a></li>
+				<li><a href="#">Pages</a>
+				<ul class="sub-menu">
+					<li><a href="portfolio-item.html">Portfolio Item</a></li>
+					<li><a href="blog-single.html">Blog Article</a></li>
+					<li><a href="shop-single.html">Shop Item</a></li>
+					<li><a href="portfolio-category.html">Portfolio Category</a></li>
+				</ul>
+				</li>
+				<li><a href="contact.php">Contact</a></li>
+			</ul>
+		</div>
+		</nav>
+		</header>
